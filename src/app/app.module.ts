3ba947233e8a2ca6772006af5480f17839c6b0de
Preetha <preethacp@gmail.com>
2022-09-23 21:33:31 +1000

    Removed sidenav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { SidenavComponent } from './home/sidenav/sidenav.component';
+import { MatTool}
 // import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SidenavComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
